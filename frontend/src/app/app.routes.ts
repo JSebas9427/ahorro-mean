bc91f0d5eec719ddc5f3b50d253f5b145553d21e
JSebas9427 <moder9427@hotmail.com>
2026-05-21 19:55:49 -0500

    Remover redirect login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,20 +1,22 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
-
 import { Proyecciones } from './pages/proyecciones/proyecciones';
 
 export const routes: Routes = [
+
   {
     path: '',
-    redirectTo: 'login',
-    pathMatch: 'full'
+    component: Login
   },
+
   {
     path: 'login',
     component: Login
   },
+
   {
     path: 'proyecciones',
     component: Proyecciones
   }
+
 ];
